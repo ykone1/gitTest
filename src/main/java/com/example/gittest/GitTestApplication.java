f14117337d21e64237f9a5f29f84f51dcb52c5e7
ykone1 <84559133+ykone1@users.noreply.github.com>
2022-08-08 16:18:01 +0800

    Update GitTestApplication.java

--- src/main/java/com/example/gittest/GitTestApplication.java
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitTestApplication {
 
     public static void main(String[] args) {
+        System.out.println("dev分支");
         SpringApplication.run(GitTestApplication.class, args);
     }
 
